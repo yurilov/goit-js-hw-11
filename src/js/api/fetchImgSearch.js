58e41b3779f30b82b1c589bc6812eb201d15300c
Yurii Shchepilov <yuraschepilov@gmail.com>
2022-02-17 12:14:10 +0200

    changed api arch

--- src/js/api/fetchImgSearch.js
+++ src/js/api/fetchImgSearch.js
@@ -1,10 +1,12 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import axios from 'axios';
-const API_KEY = '25706791-83f254c628a19fe308d2a3f92';
+import settings from './settings';
 
-axios.defaults.baseURL = 'https://pixabay.com/api/';
+const { BASE_URL, API_KEY } = settings;
 
-export async function fetchSearch(q, page, perPage = 40) {
+axios.defaults.baseURL = BASE_URL;
+
+export async function fetchSearch(q, page = 1, perPage = 40) {
   const result = await axios.get(
     `?key=${API_KEY}&per_page=${perPage}&page=${page}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true`,
   );
