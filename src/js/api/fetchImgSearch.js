c71a63e347856b58bcfc31c8a0afd3b3bd3c7a06
Yurii Shchepilov <yuraschepilov@gmail.com>
2022-02-17 20:38:33 +0200

    styles

--- src/js/api/fetchImgSearch.js
+++ src/js/api/fetchImgSearch.js
@@ -6,9 +6,14 @@ const { BASE_URL, API_KEY } = settings;
 
 axios.defaults.baseURL = BASE_URL;
 
-export async function fetchSearch(q, page = 1, perPage = 40) {
-  const result = await axios.get(
-    `?key=${API_KEY}&per_page=${perPage}&page=${page}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true`,
-  );
-  return result.data;
+export async function fetchSearch(q, page = 1, perPage = 39) {
+  try {
+    const result = await axios.get(
+      `?key=${API_KEY}&per_page=${perPage}&page=${page}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true`,
+    );
+    Notify.success(`Hurray, we found ${result.data.total}`);
+    return result.data;
+  } catch {
+    Notify.failure('Sorry, cant find such images!');
+  }
 }
